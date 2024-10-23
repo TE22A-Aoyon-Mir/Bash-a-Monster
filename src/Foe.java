9c18d93a9c231db8e22f6e225dc9493515eb5059
Aoyon Mir <aoyon.mir@elev.ga.ntig.se>
2024-10-23 10:32:47 +0200

    Bash-a-Monster done

--- src/Foe.java
+++ src/Foe.java
@@ -2,4 +2,5 @@ public class Foe {
     //Attributes
     public String name = "Foe";
     public int hp = 30;
+    public int size = 3;
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
         System.out.println("Hello world!");
 
         //Object (class name = new class())
         Player thePlayer = new Player();
         Foe theFoe = new Foe();
 
-        System.out.println(thePlayer.name + " has " + thePlayer.hp + " hp");
-        System.out.println(theFoe.name + " has " + theFoe.hp + " hp");
+        while (theFoe.hp > 0) {
+            System.out.println(thePlayer.name + " has " + thePlayer.hp + " hp");
+            System.out.println(theFoe.name + " has " + theFoe.hp + " hp and size " + theFoe.size);
 
-        System.out.println("Player attacks!");
-        theFoe.hp -= 10;
-        System.out.println(theFoe.name + " has " + theFoe.hp + " hp");
+            System.out.println("Player attacks!");
+            theFoe.hp -= 10;
+            System.out.println(theFoe.name + " now has " + theFoe.hp + " hp");
+            System.out.println("Press any key to continue");
+            keyboard.nextLine();
+        }
+        System.out.println("The foe is dead");
     }
 }
